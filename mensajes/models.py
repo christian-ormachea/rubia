e821873532067457acc3c01d59a7414816e1ada0
christian-ormachea <ormacheachristianf@gmail.com>
2026-08-03 22:39:00 -0300

    Cambios nuevos, agregar Carrusell

--- mensajes/models.py
+++ mensajes/models.py
@@ -2,7 +2,13 @@ from django.db import models
 
 class Mensaje(models.Model):
     texto = models.CharField(max_length=200)
+    descripcion = models.CharField(max_length=250, blank=True)
+    imagen = models.ImageField(upload_to='mensajes/', blank=True, null=True)
+    orden = models.PositiveIntegerField(default=0)
     activo = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['orden', 'id']
 
     def __str__(self):
         return self.texto[:50]
